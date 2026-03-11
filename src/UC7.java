e59b0d95018a9a93f15242dd38d830493092dc58
Anjaneya2005 <technicallobby1@gmail.com>
2026-03-11 20:29:49 +0530

    UC7

---
+++ src/UC7.java
@@ -0,0 +1,41 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class UC7 {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String word = "madam";
+
+        // Create Deque
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear characters
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();  // remove from front
+            char rear = deque.removeLast();    // remove from rear
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
+
+    }
+}
